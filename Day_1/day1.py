5edfe63aac66d284b5e91523862495c7e5239741
ZofiaDobrowolskaPwr <zofia.dobrowolska@pwr.edu.pl>
2026-07-21 13:37:11 +0200

    day1
    Band Name Generator:
    — a simple string manipulation (print(), input(), and early variable introduction)

---
+++ Day_1/day1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+band_name = city + " " + pet_name
+print("Your band name could be " + band_name)
